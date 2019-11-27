61c5694be6a07bbc48bbe3bc4b0ed34695d91ea3
keidu <guille.kfb@gmail.com>
2019-11-27 18:21:39 +0100

    upgrades in css

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -6,15 +6,16 @@ router.get('/', (req, res, next) => {
     res.render('search')
 });
 
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
     let search = req.body.search
-    Post.find({search})
-    .then(post => {
-        console.log(post)
-        res.render('search', {post: post})})
-    
-    .catch(err => console.log(err))
-    })
+    Post.find({ 'country': search })
+        .then(post => {
+            console.log(post)
+            res.render('search', { post: post })
+        })
+
+        .catch(err => console.log(err))
+})
 
 
 
