b33ea129c0b5a490dd43c37f5706e5be57d9e182
keidu <guille.kfb@gmail.com>
2019-11-28 11:21:23 +0100

    edit view

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post.models')
 const uploadCloud = require('../configs/cloudinary');
-const {ensureLoggedIn, ensureLoggedOut} = require('connect-ensure-login');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 // Nuevo Post: renderizar formulario
 router.get('/new', ensureLoggedIn('/auth/login'), (req, res) => {
@@ -24,14 +24,14 @@ router.post('/new', uploadCloud.single('picPath'), (req, res) => {
     } = req.body
 
     Post.create({
-            creatorId: req.user._id,
-            picPath,
-            picName,
-            location,
-            country,
-            province,
-            city
-        })
+        creatorId: req.user._id,
+        picPath,
+        picName,
+        location,
+        country,
+        province,
+        city
+    })
         .then(x => res.redirect('/auth/profile'))
         .catch(err => 'error: ' + err)
 })
@@ -44,17 +44,30 @@ router.get('/details/:id', (req, res) => {
         .then(thePost => {
             auxPost = thePost
             res.render('Post/detailPost', auxPost)
-    })
+        })
         .catch(err => console.log("Error consultando la BBDD", err))
 })
 
-router.get('/api', (req, res, next) => {res.status(200).json(auxPost)
+router.get('/api', (req, res, next) => {
+    res.status(200).json(auxPost)
 });
+
+
+router.get('/detail/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .populate('creatorId')
+        .then(thePost => {
+            res.render('Post/profileEditPost', thePost)
+        })
+        .catch(err => console.log("Error consultando la BBDD", err))
+})
 
 router.get('/delete', (req, res) => {
     Post.findByIdAndDelete(req.query.id)
         .then(() => res.redirect('/auth/profile'))
         .catch(err => console.log(err))
 })
+
+
 
 module.exports = router;
